271b00167b1d2c7790fda041f36ddeff33df88d5
Cafetera_DAW <Cafetera_DAW@DESKTOP-PS6NNQT>
2019-11-21 23:51:34 +0100

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright 2019 Lluís CP - aw.lluiscerda@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Lluís CP - aw.lluiscerda@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//referencias
+        final int CUENTA_INI = 0;
+        final int CUENTA_FIN = 10;
+
+//variables
+        int i;
+
+//inicializacion
+        i = CUENTA_INI;
+
+//BUCLE
+        while (i < CUENTA_FIN) {
+
+//mensaje salida
+            System.out.println(i);
+
+//actualizacion
+            i++;
+        }
+
+    }
+}
